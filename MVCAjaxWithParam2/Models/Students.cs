269f314e78667b7807b18c12d07e7084df88790a
DO04dev <thaboroy@gmail.com>
2022-02-23 17:13:41 +0200

    Add project files.

---
+++ MVCAjaxWithParam2/Models/Students.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace MVCAjaxWithParam2.Models
+{
+    public class Students
+    {
+        //[Key]
+        //public int studentID { get; set; }
+        //[Required]
+        //public string studentName { get; set; }
+        //[Required]
+        //public string studentAddress { get; set; }
+    }
+}
